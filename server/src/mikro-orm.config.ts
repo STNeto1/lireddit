1ed49434b38c48630c210554d15c2a72f37e6cee
STNeto1 <netofreitas7052@gmail.com>
2020-08-24 15:28:28 -0300

    register resolver & login resolver

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -3,9 +3,10 @@ import path from 'path'
 
 import { __prod__ } from './constants'
 import { Post } from './entities/Post'
+import { User } from './entities/User'
 
 export default {
-  entities: [Post],
+  entities: [Post, User],
   migrations: {
     path: path.join(__dirname, './migrations'),
     pattern: /^[\w-]+\d+\.[tj]s$/,
--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,8 +1,7 @@
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql'
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql'
 
 import { Post } from '../entities/Post'
 import { MyContext } from 'src/types'
-import { argv } from 'process'
 
 @Resolver()
 export class PostResolver {
