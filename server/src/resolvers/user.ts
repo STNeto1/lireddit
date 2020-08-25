0b6c6514dd165d8e4a11feb7fb2b1625d6297716
STNeto1 <netofreitas7052@gmail.com>
2020-08-25 00:13:39 -0300

    register error handling

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from 'type-graphql'
 import argon2 from 'argon2'
+import { EntityManager } from '@mikro-orm/postgresql'
 
 import { MyContext } from 'src/types'
 import { User } from '../entities/User'
@@ -80,17 +81,26 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password)
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    })
+
+    let user
 
     try {
-      await em.persistAndFlush(user)
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*')
+
+      user = result[0]
     } catch (error) {
       // duplicate username error
       if (error.code === '23505') {
-        //|| error.detail.includes('already exists')){
+        // error.detail.includes('already exists')) {
         return {
           errors: [
             {
