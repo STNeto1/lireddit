59f199889b72e2ed76008e33df906c628c56efd9
STNeto1 <netofreitas7052@gmail.com>
2020-08-24 23:39:01 -0300

    urql basics

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,13 +6,13 @@ import 'reflect-metadata'
 import redis from 'redis'
 import session from 'express-session'
 import connectRedis from 'connect-redis'
+import cors from 'cors'
 
 import { __prod__ } from './constants'
 import mikroConfig from './mikro-orm.config'
 import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from './resolvers/post'
 import { UserResolver } from './resolvers/user'
-import { MyContext } from './types'
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig)
@@ -22,6 +22,13 @@ const main = async () => {
 
   const RedisStore = connectRedis(session)
   const redisClient = redis.createClient()
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  )
 
   app.use(
     session({
@@ -47,10 +54,15 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   })
 
-  apolloServer.applyMiddleware({ app })
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      origin: false,
+    },
+  })
 
   app.listen(4000, () => {
     console.log('server started on http://localhost:4000')
