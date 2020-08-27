4896f751d226d564c31d7f6de9d411eef55a4495
STNeto1 <netofreitas7052@gmail.com>
2020-08-27 16:09:04 -0300

    more pagination

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -33,7 +33,7 @@ const Index = () => {
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data!.posts.map((p) => (
+          {data!.posts.posts.map((p) => (
             <Box p={5} shadow="md" borderWidth="1px" key={p.id}>
               <Heading fontSize="xl">{p.title}</Heading>
               <Text>{p.textSnippet}</Text>
@@ -42,13 +42,13 @@ const Index = () => {
         </Stack>
       )}
 
-      {data ? (
+      {data && data.posts.hasMore ? (
         <Flex>
           <Button
             onClick={() => {
               setVariables({
                 limit: variables.limit,
-                cursor: data.posts[data.posts.length - 1].createdAt,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
               })
             }}
             isLoading={fetching}
