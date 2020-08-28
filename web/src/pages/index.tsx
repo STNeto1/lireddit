9d0615b20783c9e4372c07f9126ed60302f0fa0b
STNeto1 <netofreitas7052@gmail.com>
2020-08-27 23:44:20 -0300

    invalidate queries

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -8,7 +8,7 @@ import { createUrqlClient } from '../utils/createUrqlClient'
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 10,
+    limit: 15,
     cursor: null as null | string,
   })
 
@@ -36,6 +36,8 @@ const Index = () => {
           {data!.posts.posts.map((p) => (
             <Box p={5} shadow="md" borderWidth="1px" key={p.id}>
               <Heading fontSize="xl">{p.title}</Heading>
+
+              <Text>posted by {p.creator.username}</Text>
               <Text>{p.textSnippet}</Text>
             </Box>
           ))}
